d01a2146944e20abd8e9cc6999bf69c4b8f05065
waynetian-42 <arthassum@gmail.com>
2024-09-29 15:46:01 +0800

    确定法向量的方向始终朝外

--- src/InOneWeekend/hittable.h
+++ src/InOneWeekend/hittable.h
@@ -9,6 +9,20 @@ class hit_record
     point3 p;
     vec3 normal;
     double t;
+    // ture表示光线来自外部，false表示来自内部
+    bool front_face;
+
+    /**
+     * @brief 设置交点法向量始终朝外
+     *
+     * @param r 光线
+     * @param outward_normal 朝外的单位向量
+     */
+    void set_face_normal(const ray &r, const vec3 &outward_normal)
+    {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable
